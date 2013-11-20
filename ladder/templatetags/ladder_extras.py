ce74177a8d633b4dab76d73fe954c2447d2b36b7
Jonathan Zahedieh <zahesibw@gmail.com>
2013-11-20 13:15:22 +0000

    Add averages to division view

--- ladder/templatetags/ladder_extras.py
+++ ladder/templatetags/ladder_extras.py
@@ -23,6 +23,24 @@ def gettotal(value, arg):
     except KeyError:
         return total
 
+@register.filter(name='getaverage')
+def getaverage(value, arg):
+    total = 0.0
+    count = 0.0
+    try:
+        for result in value[arg]:
+            if result.result == 9:
+                total = total + result.result + 3
+            else:
+                total = total + result.result + 1
+            count += 1
+        if count != 0:
+            return "%.2f" % (total / count)
+        return 0
+    except KeyError:
+        if count != 0:
+            return "%.2f" % (total / count)
+        return 0
 @register.filter(name='unplayed')
 def unplayed(value, arg):
     not_played = []
