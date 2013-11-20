266e8ad9838e14815df0bbb245dec7954dffe679
Jonathan Zahedieh <zahesibw@gmail.com>
2013-11-20 07:28:39 +0000

    Form validation player != opponent for result

--- ladder/forms.py
+++ ladder/forms.py
@@ -13,3 +13,21 @@ class AddResultForm(forms.ModelForm):
     class Meta:
         model = Result
         exclude = ['date_added', 'ladder']
+
+    def clean(self):
+        """
+        Validation to make sure player does not equal opponent for result
+        """
+
+        # Make sure both keys are set
+        try:
+            player = self.cleaned_data['player']
+            opponent = self.cleaned_data['opponent']
+        except KeyError:
+            # if not set return cleaned_data to allow for default validation to work
+            return self.cleaned_data
+        # Make the check
+        if player == opponent:
+            raise forms.ValidationError("Invalid Result: Player and Opponent cannot be the same person.", 'player_equals_opponent')
+        else:
+            return self.cleaned_data
