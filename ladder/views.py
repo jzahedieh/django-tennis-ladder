f8ef10a9f71ddd974b419fd297a2b25e8089d842
Jonathan Zahedieh <zahesibw@gmail.com>
2014-03-20 21:29:41 +0000

    Views PEP 8 cleanup

--- ladder/views.py
+++ ladder/views.py
@@ -29,7 +29,7 @@ def index(request):
     context = {
         'current_season': current_season,
         'at_years': (cs_year - os_year),
-        'at_years_str': ' (' + os_year.__str__() +  ' -> ' + cs_year.__str__() +  ')',
+        'at_years_str': ' (' + os_year.__str__() + ' -> ' + cs_year.__str__() + ')',
         'at_divisions': at_divisions,
         'at_ladders': at_ladders,
         'at_results': at_results,
@@ -51,52 +51,59 @@ def list_rounds(request):
 @gzip_page
 @cache_page(60 * 60, key_prefix='season')  # 1 hour page cache
 def season(request, year, season_round):
-    season = get_object_or_404(Season, start_date__year=year, season_round=season_round)
+    season_object = get_object_or_404(Season, start_date__year=year, season_round=season_round)
 
-    ladders = Ladder.objects.filter(season=season)
+    ladders = Ladder.objects.filter(season=season_object)
 
-    results = Result.objects.filter(ladder__season=season)
-    league = League.objects.filter(ladder__season=season)
+    results = Result.objects.filter(ladder__season=season_object)
+    league = League.objects.filter(ladder__season=season_object)
 
     results_dict = {}
 
     for result in results:
         results_dict.setdefault(result.player.id, []).append(result)
 
-    return render(request, 'ladder/season/index.html',
-                  dict(season=season, ladders=ladders, results_dict=results_dict, league=league)
+    return render(
+        request, 'ladder/season/index.html',
+        dict(season=season_object, ladders=ladders, results_dict=results_dict, league=league)
     )
 
 
 @gzip_page
 def ladder(request, year, season_round, division_id):
-    ladder = get_object_or_404(Ladder, division=division_id, season__start_date__year=year, season__season_round=season_round)
+    ladder_object = get_object_or_404(Ladder, division=division_id, season__start_date__year=year,
+                                      season__season_round=season_round)
 
-    results = Result.objects.filter(ladder=ladder)
+    results = Result.objects.filter(ladder=ladder_object)
 
     results_dict = {}
 
     for result in results:
         results_dict.setdefault(result.player.id, []).append(result)
 
-    return render(request, 'ladder/ladder/index.html', {'ladder': ladder, 'results_dict': results_dict})
+    return render(request, 'ladder/ladder/index.html', {'ladder': ladder_object, 'results_dict': results_dict})
+
 
 @login_required
 def add(request, year, season_round, division_id):
-    ladder = get_object_or_404(Ladder, division=division_id, season__start_date__year=year, season__season_round=season_round)
-    result = Result(ladder=ladder, date_added=datetime.datetime.now())
+    ladder_object = get_object_or_404(Ladder, division=division_id, season__start_date__year=year,
+                                      season__season_round=season_round)
+
+    result = Result(ladder=ladder_object, date_added=datetime.datetime.now())
 
     # process or generate te score adding form
     if request.POST:
-        form = AddResultForm(ladder, request.POST, instance=result)
+        form = AddResultForm(ladder_object, request.POST, instance=result)
         if form.is_valid():
             losing_result = form.save(commit=False)
 
             # check for existing results, assume update if find a match aka delete
             try:
-                existing_player_result = Result.objects.get(ladder=ladder, player=losing_result.player, opponent=losing_result.opponent)
+                existing_player_result = Result.objects.get(ladder=ladder_object, player=losing_result.player,
+                                                            opponent=losing_result.opponent)
                 existing_player_result.delete()
-                existing_opponent_result = Result.objects.get(ladder=ladder, player=losing_result.opponent, opponent=losing_result.player)
+                existing_opponent_result = Result.objects.get(ladder=ladder_object, player=losing_result.opponent,
+                                                              opponent=losing_result.player)
                 existing_opponent_result.delete()
             except Result.DoesNotExist:
                 pass
@@ -109,17 +116,18 @@ def add(request, year, season_round, division_id):
             winning_result.save()
 
             return HttpResponseRedirect(reverse('ladder:add', args=(
-                ladder.season.start_date.year, ladder.season.season_round, ladder.division)))
+                ladder_object.season.start_date.year, ladder_object.season.season_round, ladder_object.division)))
     else:
-        form = AddResultForm(ladder, instance=result)
+        form = AddResultForm(ladder_object, instance=result)
 
     # prepare the results for displaying
-    results = Result.objects.filter(ladder=ladder)
+    results = Result.objects.filter(ladder=ladder_object)
     results_dict = {}
     for result in results:
         results_dict.setdefault(result.player.id, []).append(result)
 
-    return render(request, 'ladder/ladder/add.html', {'ladder': ladder, 'results_dict': results_dict, 'form': form})
+    return render(request, 'ladder/ladder/add.html',
+                  {'ladder': ladder_object, 'results_dict': results_dict, 'form': form})
 
 
 @gzip_page
@@ -134,27 +142,30 @@ def player_history(request, player_id):
     try:
         head = Result.objects.values('opponent', 'opponent__first_name', 'opponent__last_name').filter(
             player=player).annotate(times_played=Count('opponent'), last_played=Max('date_added')).order_by(
-            '-times_played')[:10]
+                '-times_played')[:10]
     except Result.DoesNotExist:
         raise Http404
 
-    return render(request, 'ladder/player/history.html', {'player': player, 'league_set': league_set, 'ladder_set': league_set, 'head': head})
+    return render(request, 'ladder/player/history.html',
+                  {'player': player, 'league_set': league_set, 'ladder_set': league_set, 'head': head})
 
 
 @gzip_page
 def head_to_head(request, player_id, opponent_id):
-
     player = get_object_or_404(Player, pk=player_id)
     opponent = get_object_or_404(Player, pk=opponent_id)
 
-    results1 = Result.objects.filter(player=player, opponent=opponent, result__lt=9).order_by('-ladder__season__end_date')
-    results2 = Result.objects.filter(player=opponent, opponent=player, result__lt=9).order_by('-ladder__season__end_date')
+    results1 = Result.objects.filter(player=player, opponent=opponent, result__lt=9).order_by(
+        '-ladder__season__end_date')
+    results2 = Result.objects.filter(player=opponent, opponent=player, result__lt=9).order_by(
+        '-ladder__season__end_date')
 
     results = results1 | results2
 
     stats = {'won': results2.count(), 'lost': results1.count(), 'played': results1.count() + results2.count()}
 
-    return render(request, 'ladder/head_to_head/index.html', {'stats':stats, 'results': results, 'player': player, 'opponent':opponent})
+    return render(request, 'ladder/head_to_head/index.html',
+                  {'stats': stats, 'results': results, 'player': player, 'opponent': opponent})
 
 
 @gzip_page
@@ -179,7 +190,7 @@ def player_result(request):
 
 
 def player_search(request):
-    resultSet = {}
+    result_set = {}
     try:
         query = request.GET[u'query']
     except:
@@ -191,13 +202,12 @@ def player_search(request):
         qs = qs.filter(Q(first_name__icontains=term) | Q(last_name__icontains=term))
 
     results = [escape(x.first_name.strip() + ' ' + x.last_name.strip()) for x in qs]
-    resultSet["options"] = results
+    result_set["options"] = results
 
-    return HttpResponse(json.dumps(resultSet), content_type="application/json")
+    return HttpResponse(json.dumps(result_set), content_type="application/json")
 
 
 def h2h_search(request, player_id):
-
     result_set = {}
     try:
         query = request.GET[u'query']
@@ -223,25 +233,22 @@ def h2h_search(request, player_id):
 
 
 def season_ajax_stats(request):
-    try:
-        season_id = request.GET[u'id']
-    except:
+
+    season_id = request.GET.get('id', False)
+    if season_id is False:
         raise Http404
 
     try:
-        season = Season.objects.get(pk=season_id)
+        season_object = Season.objects.get(pk=season_id)
     except Season.DoesNotExist:
         raise Http404
     except ValueError:
         raise Http404
 
-    stats = season.get_stats()
+    stats = season_object.get_stats()
 
-    try:
-        include_leader = request.GET[u'leader']
-        if include_leader:
-            stats.update(season.get_leader_stats())
-    except:
-        pass
+    include_leader = request.GET.get('leader', False)
+    if include_leader:
+        stats.update(season_object.get_leader_stats())
 
     return HttpResponse(json.dumps(stats), content_type="application/json")
