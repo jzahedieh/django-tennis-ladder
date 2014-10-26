8869b5d4cfbab13a49755261c2abf69546701539
Jonathan Zahedieh <zahesibw@gmail.com>
2014-10-26 20:57:01 +0000

    Ignore logs and update dev static content delivery

--- tennis/urls.py
+++ tennis/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
+from django.conf.urls.static import static
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -7,9 +8,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
-                           {'document_root', settings.STATIC_ROOT}),
-                       url(r'^admin/', include(admin.site.urls)),
-                       url(r'', include('ladder.urls', namespace="ladder")),
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'', include('ladder.urls', namespace="ladder")),
 
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
