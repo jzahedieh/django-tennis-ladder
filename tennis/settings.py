644ced7a5964532a204387bd625f429c7edd3b3e
Jonathan Zahedieh <jzahedieh@gmail.com>
2020-09-20 00:04:52 +0100

    email default from

--- tennis/settings.py
+++ tennis/settings.py
@@ -183,6 +183,7 @@ EMAIL_PORT = os.environ.get('EMAIL_PORT')
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_DEFAULT_FROM')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 INTERNAL_IPS = (
