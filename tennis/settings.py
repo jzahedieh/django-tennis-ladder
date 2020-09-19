2aca211fc8e3828f7da6c336a2f2e0ab81477c6b
Jonathan Zahedieh <jzahedieh@gmail.com>
2020-09-19 23:47:07 +0100

    email host user

--- tennis/settings.py
+++ tennis/settings.py
@@ -181,6 +181,7 @@ LOGGING = {
 
 EMAIL_PORT = os.environ.get('EMAIL_PORT')
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
