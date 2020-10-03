2d3e1e18d163e8aab5c741088962f6707d630ee4
Jonathan Zahedieh <jzahedieh@gmail.com>
2020-10-03 13:41:28 +0100

    DRF endpoint permissions

--- ladder/api/views.py
+++ ladder/api/views.py
@@ -6,35 +6,33 @@ from ladder.api.serializers import (
     ResultSerializer
 )
 from ladder.models import Season, Player, Ladder, League, Result
-from rest_framework import viewsets
-from rest_framework import permissions
-
+from rest_framework import permissions, viewsets
 
 class SeasonViewSet(viewsets.ModelViewSet):
     queryset = Season.objects.all()
     serializer_class = SeasonSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
 
 
 class PlayerViewSet(viewsets.ModelViewSet):
     queryset = Player.objects.all()
     serializer_class = PlayerSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.DjangoModelPermissions]
 
 
 class LadderViewSet(viewsets.ModelViewSet):
     queryset = Ladder.objects.all()
     serializer_class = LadderSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
 
 
 class LeagueViewSet(viewsets.ModelViewSet):
     queryset = League.objects.all()
     serializer_class = LeagueSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
 
 
 class ResultViewSet(viewsets.ModelViewSet):
     queryset = Result.objects.all()
     serializer_class = ResultSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAdminUser]
