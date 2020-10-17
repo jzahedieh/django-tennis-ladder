c47da750ab0f6c4a0d0ae42c2aa7eb19daf594b0
Jonathan Zahedieh <jzahedieh@gmail.com>
2020-10-17 16:50:33 +0100

    drf-simplejwt integration

--- tennis/urls.py
+++ tennis/urls.py
@@ -2,6 +2,10 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 admin.autodiscover()
 
@@ -11,6 +15,8 @@ urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('api/auth/', include('rest_framework.urls')),
                   path('api/', include('ladder.api.urls')),
+                  path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+                  path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
