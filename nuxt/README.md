be1c15b7bc9bf3177f46530ea377d3c6c929ea2f
Jonathan Zahedieh <jzahedieh@gmail.com>
2020-10-17 12:04:55 +0100

    layout + navigation culling

--- nuxt/README.md
+++ nuxt/README.md
@@ -18,3 +18,9 @@ $ yarn generate
 ```
 
 For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
+
+## Development Enviroment
+
+Recommended Tools:
+* VSCode
+    * Vetur Extension
--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -1,12 +1,6 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
+  <v-app>
+    <v-navigation-drawer v-model="drawer" fixed app>
       <v-list>
         <v-list-item
           v-for="(item, i) in items"
@@ -24,37 +18,12 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
+    <v-app-bar fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
+      <v-btn icon>
+        <v-icon>mdi-account</v-icon>
       </v-btn>
     </v-app-bar>
     <v-main>
@@ -62,28 +31,15 @@
         <nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+    <v-footer padless>
+      <v-col class="text-center" cols="12">
+        <a href="mailto:feedback@highgate-ladder.co.uk" target="_top">
+          feedback@highgate-ladder.co.uk
+        </a>
+      </v-col>
+      <v-col class="text-center" cols="12">
+        Want to contribute? <a href="https://github.com/jzahedieh/django-tennis-ladder" target="_blank">Github</a>
+      </v-col>
     </v-footer>
   </v-app>
 </template>
@@ -92,25 +48,26 @@
 export default {
   data () {
     return {
-      clipped: false,
       drawer: false,
       fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
+          icon: 'mdi-tennis',
+          title: 'Overview',
           to: '/'
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
+          icon: 'mdi-tennis-ball',
+          title: 'Season',
+          to: '/inspire'
+        },
+        {
+          icon: 'mdi-tennis-ball',
+          title: 'Ladder',
           to: '/inspire'
         }
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Highgate Ladder Tracker'
     }
   }
 }
