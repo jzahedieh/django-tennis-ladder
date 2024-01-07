2e715765f0cd318cabd1c2dd83c8d8f46c9ef53f
Jonathan Zahedieh <jzahedieh@gmail.com>
2024-01-07 13:46:31 +0000

    System and code dependancy updates, daily mysql backup cron
    - mariadb: 10.3 -> 10.11 (run mysql_upgrade after deployment)
    - python: 3.11 -> 3.2
    
    - django: 4.1 -> 4.2 LTS
    - mysqlclient: 2.1.1 -> 2.2.1

--- docker/db/backup.sh
+++ docker/db/backup.sh
@@ -6,7 +6,7 @@ cd /backup/ || exit
 filename="${SQL_DATABASE}"_"$(date +%F_%T)".sql
 
 echo "export started $filename"
-mysqldump -u"$SQL_USER" -p"$SQL_PASSWORD" "$SQL_DATABASE" > "$filename"
+mysqldump --user=root --password="${MYSQL_ROOT_PASSWORD}" "${SQL_DATABASE}" > $filename
 echo "export finished: $filename"
 
 # clean up old exports, does not support filename spaces
